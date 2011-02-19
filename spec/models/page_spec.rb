65a829fe098afa643707e47c7254add4703726a8
Yosem Sweet <yosem.sweet@suite101.com>
2011-02-19 13:14:51 -0800

    Added growl notifications to autotest

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 describe Page do
   it "should have a url" do
-		p  = Page.new
+		p = Page.new
 		p.should respond_to(:url)
+	end
+	
+	it "should have a description" do
+		p = Page.new
+		p.should respond_to(:description)
 	end
 end
