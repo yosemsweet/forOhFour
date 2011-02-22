93c017f34683e16f23bdd6d5e004cb7ee65541f4
Yosem Sweet <yosem.sweet@suite101.com>
2011-02-21 23:12:24 -0800

    Tests implemented for 404 and 200 status codes for URL Validator

--- lib/url_return_code_validator.rb
+++ lib/url_return_code_validator.rb
@@ -2,19 +2,31 @@ require 'net/http'
 
 class UrlReturnCodeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-		uri = URI.parse("http://www.google.ca/")
-
-		http = Net::HTTP.new(uri.host, uri.port)
-		request = Net::HTTP::Get.new(uri.request_uri)
-
-		response = http.request(request)
+		# puts "Validating #{value} returns #{options[:status]}"
 		
-		errors = record.errors[attribute] << "URL must return #{options[:status]} - returned #{response.code}" unless response.code.to_i == options[:status]
-		puts errors
-		puts response.code
-		puts value
-		puts attribute
-		puts options[:status]
-		errors
+		begin
+			uri = URI.parse(value)
+
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Get.new(uri.request_uri)
+
+			response = http.request(request)
+			status = response.code.to_i
+
+			# puts response
+		rescue
+			status = -1
+		end
+		record.errors[attribute] << "URL must return #{options[:status]} - returned #{status}" unless status == options[:status]
+		
+			# puts '----------'
+			# puts record.errors
+			# puts status
+			# puts value
+			# puts attribute
+			# puts options[:status]
+			# puts '----------'
+
+		record.errors
   end
 end
--- spec/lib/url_return_code_validator_spec.rb
+++ spec/lib/url_return_code_validator_spec.rb
@@ -6,7 +6,7 @@ class Code200Page < Page
 end
 
 class Code404Page < Page
-	validate :url, :url_return_code => { :status => 404}
+	validates :url, :url_return_code => { :status => 404}
 end
 
 
@@ -18,14 +18,41 @@ describe 'UrlReturnCodeValidator' do
 	
 	context "with 200 codes" do
 	
-		it "should pass with a url return 200 code" do
+		it "should pass with a url returning a 200 code" do
 			testPage = Code200Page.new({:url => 'http://localhost:3000/'})
-			puts testPage.errors
-			
 			testPage.should be_valid
-
+		end
+		
+		context "with edge cases" do
+			it "should fail with a 404 page" do
+				testPage = Code200Page.new({:url => 'http://localhost:3000/SDKAJSBFASDAKJSD/Invalid.html'})
+				testPage.should be_invalid
+			end
+			 
+			it "should fail with a nonexistant domain" do
+				testPage = Code200Page.new({:url => 'http://invalidurl/'})
+				testPage.should be_invalid
+			end
 		end
 	end
 	
+	context "with 404 codes" do
 	
+		it "should pass with a url returning a 404 code" do
+			testPage = Code404Page.new({:url => 'http://localhost:3000/SDKAJSBFASDAKJSD/Invalid.html'})
+			testPage.should be_valid
+		end
+		
+		context "with edge cases" do
+			it "should fail with a url returning 200 code" do
+				testPage = Code404Page.new({:url => 'http://localhost:3000/'})
+				testPage.should be_invalid
+			end
+			 
+			it "should fail with a nonexistant domain" do
+				testPage = Code404Page.new({:url => 'http://invalidurl/'})
+				testPage.should be_invalid
+			end
+		end
+	end
 end
